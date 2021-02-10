e91c0819fe3cb79a5cb3ddb6e31d3973a06ce396
Alvaro Padilla <varoboss.ap@gmail.com>
2021-02-10 12:58:32 -0600

    Add logic to handle requests

--- hack3rnews.gemspec
+++ hack3rnews.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
-  s.name        = "hack3rnews"
-  s.version     = "0.0.0"
-  s.authors     = ["Alvaro Padilla"]
-  s.date        = %q{2021-02-10}
-  s.summary     = 'Consume hackernews content (stories, comments, etc)'
-  s.description = "Consume heckernews content through the use of their API"
-  s.email       = 'varoboss.ap@gmail.com'
-  s.files       = ['README', 'lib/hack3rnews.rb', 'spec/hack3rnews_spec.rb']
-  s.homepage    = 'https://github.com/ese-varo'
-  s.licens      = 'MIT'
+  s.name           = "hack3rnews"
+  s.version        = "0.0.0"
+  s.authors        = ["Alvaro Padilla"]
+  s.date           = %q{2021-02-10}
+  s.summary        = 'Consume hackernews content (stories, comments, etc)'
+  s.description    = "Consume heckernews content through the use of their API"
+  s.email          = 'varoboss.ap@gmail.com'
+  s.files          = ['README', 'lib/hack3rnews.rb', 'spec/hack3rnews_spec.rb']
+  s.homepage       = 'https://github.com/ese-varo'
+  s.licens         = 'MIT'
   s.add_dependency = 'rest-client'
+  s.add_dependency = 'json'
 end
--- lib/hack3rnews.rb
+++ lib/hack3rnews.rb
@@ -1,14 +1,21 @@
 require 'rest-client'
+require 'json'
 
-class HackerNews
-  URL = 'https://hacker-news.firebaseio.com/v0'.freeze
+class HackerNew
+  URL = 'https://hacker-news.firebaseio.com/v0/'.freeze
+  JSON_FORMAT = '/.json?print=pretty'.freeze
 
   def initialize
     @stories = []
   end
 
   def new_stories(num = 20)
-
+    new_stories_ids = request_bunch('newstories', num)
+    # while story_id = new_stories_ids.shift
+    #   @stories << request('item', story_id)
+    # end
+    @stories << request_item('item', new_stories_ids[0])
+    # @stories
   end
 
   def top_stories(num = 20)
@@ -26,4 +33,26 @@ class HackerNews
   def top_job_stories(num = 20)
 
   end
+
+  private
+  def request(url)
+    response = RestClient.get(url)
+    JSON.parse(response)
+  end
+
+  def request_item(type, id)
+    url = URL + type + '/' + id.to_s + JSON_FORMAT
+    request(url)
+  end
+
+  def request_bunch(type, num)
+    url = URL + type + JSON_FORMAT
+    response = request(url)
+    response.first(num)
+  end
 end
+=begin
+  - item, user, maxitem
+  - topstories, newstories, beststories
+  - askstories, showstories, jobstories
+=end
